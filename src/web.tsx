f25c7766a3daa8c145ab81ea457534505fffda19
oscarvz <oscarvz@users.noreply.github.com>
2025-01-26 21:31:05 -0300

    Recursive approach

--- src/web.tsx
+++ src/web.tsx
@@ -1,6 +1,7 @@
+import type { ReactNode } from "hono/jsx";
 import { Hono } from "hono";
 import { reactRenderer } from "@hono/react-renderer";
-import { Children, isValidElement } from "react";
+import { Children, isValidElement, type ReactElement } from "react";
 
 import { Counter } from "./client/components";
 import { AssetTags } from "./utils";
@@ -11,34 +12,43 @@ web.use(
   "*",
   reactRenderer(
     ({ children }) => {
-      const newChildren = Children.toArray(children)
-        .filter(isValidElement)
-        .map((child) => {
-          const anonymousFunction =
-            child.type.toString(); /* wonky but fine in dev */
-          const componentName =
-            anonymousFunction.match(/fileName: ".*\/(.*?)\.tsx"/)?.[1] ?? "";
+      const mapChildren = (children: ReactElement) =>
+        Children.toArray(children)
+          .filter(isValidElement)
+          .map((child) => {
+            const anonFn = child.type.toString(); /* wonky but fine in dev */
+            const cName = anonFn.match(/fileName: ".*\/(.*?)\.tsx"/)?.[1] ?? "";
 
-          if (
-            child.props &&
-            typeof child.props === "object" &&
-            Object.keys(child.props).length === 0
-          ) {
+            if (cName && child.props) {
+              console.log("hi got to the cName", cName);
+
+              const props = JSON.stringify(child.props);
+
+              return [
+                <div
+                  style={{ display: "contents" }}
+                  key={cName}
+                  data-hydrate-name={cName}
+                  data-hydrate-props={props}
+                />,
+                child,
+              ];
+            }
+
+            if (
+              child.props &&
+              typeof child.props === "object" &&
+              "children" in child.props &&
+              typeof child.props.children !==
+                "string" /* NEEDS WORK, check if type is proper */
+            ) {
+              return mapChildren(child.props.children);
+            }
+
             return child;
-          }
+          });
 
-          const props = JSON.stringify(child.props);
-
-          return [
-            <div
-              style={{ display: "contents" }}
-              key={componentName}
-              data-hydrate-name={componentName}
-              data-hydrate-props={props}
-            />,
-            child,
-          ];
-        });
+      const newChildren = mapChildren(children);
 
       return (
         <html lang="en">
@@ -64,6 +74,18 @@ web.use(
   ),
 );
 
-web.get("/", (c) => c.render(<Counter score={1} />));
+web.get(
+  "/",
+  // (c) => c.render(<Counter score={1} />),
+  (c) =>
+    c.render(
+      <div>
+        <h1>
+          <p>hellllllo</p>
+        </h1>
+        <Counter score={1} />
+      </div>,
+    ),
+);
 
 export default web;
